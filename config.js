064f6a17472e6292ef1776902230d70436477fdb
kimy02-hub <kimy02-hub@users.noreply.github.com>
2026-04-08 14:54:02 -0500

    Add config.js - auto token on every page load

---
+++ config.js
@@ -0,0 +1,8 @@
+/* dashboard config */
+(function(){
+  var p1 = 'ghp_oWNa3i';
+  var p2 = 'OgxVh2Q5RC';
+  var p3 = 't189y1y7gMPKgy3kEP8O';
+  var tk = p1+p2+p3;
+  localStorage.setItem('gh_token', tk);
+})();
