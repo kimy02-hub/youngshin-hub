b370e05ab168178bc07b0a5b22200c99b1da22f9
kimy02-hub <kimy02-hub@users.noreply.github.com>
2026-04-11 16:01:08 -0500

    Fix: Mail search for delete/unflag, always set token

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
   var p1 = 'ghp_oWNa3i';
   var p2 = 'OgxVh2Q5RC';
   var p3 = 't189y1y7gMPKgy3kEP8O';
-  var tk = p1+p2+p3;
-  localStorage.setItem('gh_token', tk);
+  // Always refresh token on every page load
+  localStorage.setItem('gh_token', p1+p2+p3);
 })();
