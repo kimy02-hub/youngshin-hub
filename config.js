bc4118dec7d1580f57c55d2a2edd4ef2cba1f1ab
kimy02-hub <kimy02-hub@users.noreply.github.com>
2026-04-20 11:13:10 -0500

    Restore: config.js simple, main.js in index.html

--- config.js
+++ config.js
@@ -5,10 +5,3 @@
   var p3 = 't189y1y7gMPKgy3kEP8O';
   localStorage.setItem('gh_token', p1+p2+p3);
 })();
-
-// Load main.js by creating a script tag - runs in global scope
-(function() {
-  var s = document.createElement('script');
-  s.src = 'main.js?t=' + Date.now();
-  document.head.appendChild(s);
-})();
